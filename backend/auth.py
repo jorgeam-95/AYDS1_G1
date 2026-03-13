b7293751d17f295971c4d2f1e4549cd685810576
Halex07 <92344208+Halex07@users.noreply.github.com>
2026-03-12 20:54:12 -0600

    Add files via upload

--- backend/auth.py
+++ backend/auth.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
-
+from security import verify_password
 from db import get_db
 from models import Patient
 from schemas import PatientRegister
@@ -11,8 +11,8 @@ router = APIRouter(prefix="/api/auth", tags=["Auth"])
 
 @router.post("/register-patient", status_code=status.HTTP_201_CREATED)
 def register_patient(data: PatientRegister, db: Session = Depends(get_db)):
-    existing_email = db.query(Patient).filter(Patient.correo == data.correo).first()
-    if existing_email:
+    Texisting_email = db.query(Patient).filter(Patient.correo == data.correo).first()
+    if Texisting_email:
         raise HTTPException(status_code=400, detail="El correo ya está registrado")
 
     existing_dpi = db.query(Patient).filter(Patient.dpi == data.dpi).first()
@@ -49,5 +49,32 @@ def register_patient(data: PatientRegister, db: Session = Depends(get_db)):
             "correo": new_patient.correo,
             "aprobado": new_patient.aprobado,
             "activo": new_patient.activo
+        }
+    }
+
+@router.post("/login")
+def login(correo: str, password: str, db: Session = Depends(get_db)):
+
+    patient = db.query(Patient).filter(Patient.correo == correo).first()
+
+    if not patient:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Credenciales incorrectas"
+        )
+
+    if not verify_password(password, patient.password_hash):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Credenciales incorrectas"
+        )
+
+    return {
+        "message": "Login exitoso",
+        "patient": {
+            "id": patient.id,
+            "nombre": patient.nombre,
+            "apellido": patient.apellido,
+            "correo": patient.correo
         }
     }
